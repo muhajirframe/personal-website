434756ef4109565d3e2db084c05bfdcdd566dd52
Muhammad Muhajir <muhajirframe@gmail.com>
2017-07-22 18:24:25 +0700

    inital commit

---
+++ src/layouts/index.js
@@ -0,0 +1,27 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
+
+import "../css/typography.css"
+
+export default class Template extends React.Component {
+  static propTypes = {
+    children: PropTypes.func,
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet
+          title="Muhammad Muhajir | muhajirframe"
+          meta={[
+            { name: "description", content: "Muhammad Muhajir's personal website" },
+            { name: "keywords", content: "web developer, front end developer, fullstack developer"},
+          ]}
+        />
+          {this.props.children()}
+      </div>
+    )
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React from "react"
+import Link from "gatsby-link"
+import Helmet from "react-helmet"
+
+export default () => (
+  <div style={{
+    fontFamily: 'monospace',
+    minHeight: '100vh',
+    textAlign: 'center',
+    paddingTop: '13rem'
+  }}>
+    <h1 style={{
+      'font-family': 'monospace'
+    }}>Muhammad Muhajir</h1>
+    <div>Front End Developer</div>
+    <div>
+      <a href="https://twitter.com/_muhajir_" style={{
+        display: 'inline-block',
+        padding: '0 1rem',
+        color: '#555'
+      }}>twitter</a>
+      <a href="https://github.com/muhajirframe" style={{
+        display: 'inline-block',
+        padding: '0 1rem',
+        color: '#555'
+      }}>github</a>
+      <a href="https://linkedin.com/muhammad-muhajir" style={{
+        display: 'inline-block',
+        padding: '0 1rem',
+        color: '#555'
+      }}>linkedIn</a>
+    </div>
+  </div>
+)
